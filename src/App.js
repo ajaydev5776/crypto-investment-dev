32409c2f49ba58c1e0dc0cfb2e7c4a3111e13410
ajaydev5776 <121281220+ajaydev5776@users.noreply.github.com>
2024-03-17 18:28:14 +0530

    setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Homepage from './pages/Home/Index';
+import './App.css';
+
+function App() {
+  return (
+   <>
+   <Homepage/>
+   </>
+  );
+}
+
+export default App;
